e2c52a12b398fc25e49fae3112962b712218836a
fairuzulum <usleunmdi32@gmail.com>
2025-07-21 09:36:31 +0700

    update font

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -25,7 +25,7 @@ const STRAPI_BASE_URL = "https://strapi.fairuzulum.me";
         >
           <h3
             class="text-sm sm:text-xl font-semibold text-center px-4"
-            style="color: rgba(255, 255, 255, 0.5)"
+            style="color: rgba(255, 255, 255, 0.65)"
           >
             {{ product.name }}
           </h3>
